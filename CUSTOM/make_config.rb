f66670e3203b809b5608b50465d76338553a205e
Brad Olson <brad.olson@movedbylight.com>
2015-10-09 23:36:25 +0000

    I stripped down the fluentd config to just output stuff to stdout.
    It's ready for some hack testing.
    
    I also just learned that ruby string interpolation works within the config.

--- CUSTOM/make_config.rb
+++ CUSTOM/make_config.rb
@@ -1,13 +1,11 @@
 require 'net/http'
 
 # this is how you read from etcd, but the address is from hosts docker0
-port = Net::HTTP.get(URI('http://172.17.42.1:4001/v2/keys/fluentd/fluentd_port'))
+#port = Net::HTTP.get(URI('http://172.17.42.1:4001/v2/keys/fluentd/fluentd_port'))
 #configuration_filename = '/CUSTOM/fluentd.conf'  
 configuration = <<HEREDOC
 <source>
   @type  forward
-  @id    input1
-  @label @mainstream
   port  24224
 </source>
 
@@ -15,28 +13,6 @@ configuration = <<HEREDOC
   @type stdout
 </filter>
 
-<label @mainstream>
-  <match docker.**>
-    @type file
-    @id   output_docker1
-    path         /fluentd/log/docker.*.log
-    symlink_path /fluentd/log/docker.log
-    append       true
-    time_slice_format %Y%m%d
-    time_slice_wait   1m
-    time_format       %Y%m%dT%H%M%S%z
-  </match>
-  <match **>
-    @type file
-    @id   output1
-    path         /fluentd/log/data.*.log
-    symlink_path /fluentd/log/data.log
-    append       true
-    time_slice_format %Y%m%d
-    time_slice_wait   10m
-    time_format       %Y%m%dT%H%M%S%z
-  </match>
-</label>
 HEREDOC
 
 #IO.write configuration_filename, configuration
