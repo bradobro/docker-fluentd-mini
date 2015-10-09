d55b8396242c003e283937dc57a7868cede81e01
Brad Olson <brad.olson@movedbylight.com>
2015-10-09 21:13:56 +0000

    Sending configuration to stdout.

--- CUSTOM/make_config.rb
+++ CUSTOM/make_config.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 # this is how you read from etcd, but the address is from hosts docker0
 port = Net::HTTP.get(URI('http://172.17.42.1:4001/v2/keys/fluentd/fluentd_port'))
-configuration_filename = '/CONTAINER/fluentd.conf'  
+#configuration_filename = '/CUSTOM/fluentd.conf'  
 configuration = <<HEREDOC
 <source>
   @type  forward
@@ -39,5 +39,6 @@ configuration = <<HEREDOC
 </label>
 HEREDOC
 
-IO.write configuration_filename, configuration
+#IO.write configuration_filename, configuration
+puts configuration
 
